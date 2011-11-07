119a38fad08dc80489ac37eb5210627387ee39f6
frederic garel <fgarel@free.fr>
2011-11-07 15:09:44 +0100

    ajout de quelques autres scritps

--- usr/sbin/fgaptitudedoall.sh
+++ usr/sbin/fgaptitudedoall.sh
@@ -2,10 +2,10 @@
 
 
 # chainage avec les scripts de configuration et les mises à jour
-export http_proxy=http://10.5.10.23:3128/
-export https_proxy=http://10.5.10.23:3128/
-#fgaptitudeupdate.sh
-#fgaptitudeclean.sh
+#export http_proxy=http://10.5.10.23:3128/
+#export https_proxy=http://10.5.10.23:3128/
+fgaptitudeupdate.sh
+fgaptitudeclean.sh
 fgaptitudeinstallsys.sh
 #fgaptitudeinstallvim.sh
 #fgaptitudeupgrade.sh
@@ -13,7 +13,7 @@ fgaptitudeinstallsys.sh
 #sudo -u garel fgaptitudeinstallpythonfabric.sh ; cd $OLDPWD
 #sudo -u garel fgaptitudeinstallpythonpyramid.sh ; cd $OLDPWD
 #sudo -u garel fgaptitudeinstallpythonmapnik.sh ; cd $OLDPWD
-sudo -u garel fgaptitudeinstallpythongeonode.sh ; cd $OLDPWD
+#sudo -u garel fgaptitudeinstallpythongeonode.sh ; cd $OLDPWD
 #fgaptitudeinstallgis.sh
 
 
@@ -25,6 +25,6 @@ sudo -u garel fgaptitudeinstallpythongeonode.sh ; cd $OLDPWD
 # - quelques utilitiares
 # - quelques fichiers de configurations
 echo "Récupération des fichiers depuis la machine distante - clefUsb2"
-scp -q -r root@10.5.10.23:/media/clefUsb2/\* /
-chown -R garel:garel /home/garel/
+#scp -q -r root@10.5.10.23:/media/clefUsb2/\* /
+#chown -R garel:garel /home/garel/
 #sudo -u garel fgaptitudeinstallgis2.sh ; cd $OLDPWD
