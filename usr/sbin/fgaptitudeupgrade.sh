095fcba5c0bf033379540f0930db6404e8ff1fc6
frederic garel <fgarel@free.fr>
2012-01-05 17:29:56 +0100

    debug

--- usr/sbin/fgaptitudeupgrade.sh
+++ usr/sbin/fgaptitudeupgrade.sh
@@ -12,8 +12,21 @@ echo y | aptitude remove xserver-xorg \
                          xserver-xorg-input-evdev \
                          virtualbox-ose-guest-x11 #1> /dev/null
 # suppression de dkms
-echo "Suppression de dkms"
-aptitude remove dkms
+echo "Suppression de dkms et virtualbox-guest"
+echo y | aptitude remove dkms \
+                         virtualbox-guest-dkms \
+                         virtualbox-guest-utils \
+                         virtualbox-guest-x11/unstable #1> /dev/null
+
+# Mise à jour du noyau et ajout de dkms
+echo "Mise à jour du noyau"
+echo y | aptitude install linux-image-486/testing \
+                          linux-base/testing \
+                          linux-headers-486/testing \
+                          dkms #1> /dev/null
+aptitude remove linux-image-686 #1> /dev/null 
+aptitude remove linux-image-2.6-686 #1> /dev/null
+
 
 # aptitude upgrade
 
@@ -24,16 +37,8 @@ aptitude remove dkms
 
 # aptitude full-upgrade
 echo "Aptitude full-upgrade"
-echo y | aptitude full-upgrade 1> /dev/null
+echo y | aptitude full-upgrade #1> /dev/null
 echo "... tude full-upgrade done"
-
-# Mise à jour du noyau  et ajout de dkms
-echo "Mise à jour du noyau"
-echo y | aptitude install linux-image-486/testing \
-                          linux-base/testing \
-                          linux-headers-486/testing \
-                          dkms
-aptitude remove linux-image-686
 
 # installation de xserver-xorg
 echo "Installation de xserver-xorg"
@@ -41,7 +46,7 @@ echo y | aptitude install xserver-common/experimental \
                           xserver-xorg-core/testing \
                           xserver-xorg-video-fbdev/testing \
                           libxfont1/squeeze-backports \
-                          xinit
+                          xinit #1> /dev/null
 
 # ajout de dkms et de virtualbox-ose-guest
 echo "Installation de virtualbox et dkms"
