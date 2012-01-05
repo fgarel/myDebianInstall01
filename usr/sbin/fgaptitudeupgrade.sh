caf66b2a697921921fb288fc138b14930130e748
frederic garel <fgarel@free.fr>
2012-01-05 12:23:00 +0100

    debug

--- usr/sbin/fgaptitudeupgrade.sh
+++ usr/sbin/fgaptitudeupgrade.sh
@@ -1,18 +1,50 @@
 #!/bin/sh
 
 # Mise à jour du noyau
-echo "Mise à jour du noyau"
-echo y | aptitude install linux-image-2.6-486 1> /dev/null
-aptitude remove linux-image-2.6-686 1> /dev/null
+#echo "Mise à jour du noyau"
+#echo y | aptitude install linux-image-2.6-486 1> /dev/null
+#aptitude remove linux-image-2.6-686 1> /dev/null
+
+# suppression de xorg"
+echo "Suppression de xserver-xorg"
+echo y | aptitude remove xserver-xorg \
+                         xserver-xorg-core \
+                         xserver-xorg-input-evdev \
+                         virtualbox-ose-guest-x11 #1> /dev/null
+# suppression de dkms
+echo "Suppression de dkms"
+aptitude remove dkms
 
 # aptitude upgrade
 
 # aptitude safe-upgrade
-echo "Aptitude safe-upgrade"
+#echo "Aptitude safe-upgrade"
 #echo y | aptitude safe-upgrade 1> /dev/null
-echo "... tude safe-upgrade done"
+#echo "... tude safe-upgrade done"
 
 # aptitude full-upgrade
 echo "Aptitude full-upgrade"
 echo y | aptitude full-upgrade 1> /dev/null
 echo "... tude full-upgrade done"
+
+# Mise à jour du noyau  et ajout de dkms
+echo "Mise à jour du noyau"
+echo y | aptitude install linux-image-486/testing \
+                          linux-base/testing \
+                          linux-headers-486/testing \
+                          dkms
+aptitude remove linux-image-686
+
+# installation de xserver-xorg
+echo "Installation de xserver-xorg"
+echo y | aptitude install xserver-common/experimental \
+                          xserver-xorg-core/testing \
+                          xserver-xorg-video-fbdev/testing \
+                          libxfont1/squeeze-backports
+
+# ajout de dkms et de virtualbox-ose-guest
+echo "Installation de virtualbox et dkms"
+echo y | aptitude install dkms \
+                          virtualbox-guest-dkms \
+                          virtualbox-guest-utils/unstable \
+                          virtualbox-guest-x11/unstable
