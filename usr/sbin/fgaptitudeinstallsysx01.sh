32ffbb85c6ab0c4226096f8aa8686ed709bbdfb3
frédéric garel <frederic.garel@gmail.com>
2012-02-09 07:29:23 +0100

    modif de fgaptitudeinstallsysx01.sh

--- usr/sbin/fgaptitudeinstallsysx01.sh
+++ usr/sbin/fgaptitudeinstallsysx01.sh
@@ -8,20 +8,23 @@ echo "Installation de systeme-X (1ère partie)"
 
 # suppression de xorg"
 echo "Suppression de xserver-xorg"
-echo y | aptitude remove xserver-xorg \
-                         xserver-xorg-core \
-                         xserver-xorg-input-evdev \
-                         virtualbox-ose-guest-x11 #1> /dev/null
+#echo y | aptitude remove xserver-xorg \
+#                         xserver-xorg-core \
+#                         xserver-xorg-input-evdev \
+#                         virtualbox-ose-guest-x11 #1> /dev/null
 
 # installation de xserver-xorg
 echo "Installation de xserver-xorg"
+#echo y | aptitude install xserver-common/experimental \
+#                          xserver-xorg-core/testing \
+#                          xserver-xorg-video-fbdev/testing \
+#                          xserver-xorg-input-evdev/testing \
+#                          libxfont1/squeeze-backports \
+#                          xinit #1> /dev/null
+
 echo y | aptitude install xserver-common/experimental \
                           xserver-xorg-core/testing \
-                          xserver-xorg-video-fbdev/testing \
-                          xserver-xorg-input-evdev/testing \
-                          libxfont1/squeeze-backports \
                           xinit #1> /dev/null
-
 
 # urxvt
 # http://www.planet-libre.org/?post_id=11153
