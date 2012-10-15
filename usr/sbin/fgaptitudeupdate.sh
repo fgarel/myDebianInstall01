11ea27eccfdc97674888e893b1a977b3c036a22a
Frederic Garel <frederic.garel@gmail.com>
2012-10-15 16:06:18 +0200

    modif fgaptitudeupdate.sh

--- usr/sbin/fgaptitudeupdate.sh
+++ usr/sbin/fgaptitudeupdate.sh
@@ -7,6 +7,8 @@ echo "Nettoyage 1"
 rm -f /var/lib/apt/lists/partial/*
 rmdir /var/lib/apt/lists/partial
 rm -f /var/lib/apt/lists/*
+
+mkdir /var/log/fg
 #aptitude clean
 # si le problème est :
 # Reading package lists... Error!
@@ -19,14 +21,15 @@ rm -f /var/lib/apt/lists/*
 # TODO: à mettre dans un bloc if pour eviter de relancer cette install à chaque
 #       fois...
 # installation du trousseau de clefs deb-multimedia-keyring
-if [ ! -e /var/log/fg/deb-multimedia-keyring ]
+if [ ! -e /var/log/fg/deb-multimedia-keyring.log ]
 then
 
   echo "Installation du trousseau de clefs deb-multimedia-keyring"
-  aptitude update 1> /dev/null #2> /dev/null
+  aptitude update 1> /dev/null 2> /dev/null
   echo "Oui" | aptitude install deb-multimedia-keyring > /dev/null
-  mkdir /var/log/fg
-  touch /var/log/fg/deb-multimedia-keyring
+  #mkdir /var/log/fg
+  #touch /var/log/fg/deb-multimedia-keyring.log
+  date +"%F %T" >> /var/log/fg/deb-multimedia-keyring.log
 
 fi
 
@@ -36,12 +39,13 @@ echo "Aptitude update"
 #cat log.txt | grep "Erreur"
 #cat erreur.txt | grep ""
 #aptitude update | grep -v "Ign" | grep -v "Atteint" &> /dev/null
+#mkdir /var/log/fg
 aptitude update 1> /dev/null 2> /dev/null
-date +"%F %T" > /var/log/fg/fgaptitudeupdate.log
+date +"%F %T" >> /var/log/fg/fgaptitudeupdate.log
 echo "... tude update done"
 
 #aptitude reinstall man-db 1> /dev/null
-aptitude remove man-db
+aptitude remove man-db 1> /dev/null 2> /dev/null
 #aptitude reinstall libconfig1 1> /dev/null
 #aptitude reinstall fontconfig-config 1> /dev/null
 
