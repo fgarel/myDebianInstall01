cc433a01540ea0ed8b0d7623ceb27b5c9af388a2
root <root@debian.mairie.fr>
2012-10-24 17:06:14 +0200

    fgaptitudeclean.sh

--- usr/sbin/fgaptitudeclean.sh
+++ usr/sbin/fgaptitudeclean.sh
@@ -14,7 +14,7 @@ if [ ! -e /var/log/fg/fgaptitudeclean-purge.log ]
 then
   echo "  Aptitude purge"
   echo y | aptitude purge "~c" 1> /dev/null 2> /dev/null
-  date +"%F %T" >> /var/log/fg/fgaptitudeclean-clean.log
+  date +"%F %T" >> /var/log/fg/fgaptitudeclean-purge.log
 fi
 
 # on enleve quelques paquets qui ne seront pas utilisés
@@ -28,14 +28,19 @@ fi
 #update-alternatives --all
 # par exemple, au lieu de désinstaller le paquet "util-linux" (qui contient pg et more)
 # http://pwet.fr/man/linux/administration_systeme/update_alternatives
-update-alternatives --remove editor /bin/nano 1> /dev/null
-update-alternatives --remove editor /usr/bin/vim.basic 1> /dev/null
-update-alternatives --remove editor /usr/bin/vim.tiny 1> /dev/null
-update-alternatives --remove fakeroot /usr/bin/fakeroot-tcp 1> /dev/null
-update-alternatives --remove pager /usr/bin/pg 1> /dev/null
-update-alternatives --remove pager /bin/more 1> /dev/null
-update-alternatives --remove vi /usr/bin/vim.tiny 1> /dev/null
-update-alternatives --remove view /usr/bin/see 1> /dev/null
-update-alternatives --remove view /usr/bin/vim.tiny 1> /dev/null
-update-alternatives --remove vim /usr/bin/vim.basic 1> /dev/null
-update-alternatives --remove vimdiff /usr/bin/vim.basic 1> /dev/null
+if [ ! -e /var/log/fg/fgaptitudeclean-alternatives.log ]
+then
+  echo "  Alternatives"
+  update-alternatives --remove editor /bin/nano 1> /dev/null
+  update-alternatives --remove editor /usr/bin/vim.basic 1> /dev/null
+  update-alternatives --remove editor /usr/bin/vim.tiny 1> /dev/null
+  update-alternatives --remove fakeroot /usr/bin/fakeroot-tcp 1> /dev/null
+  update-alternatives --remove pager /usr/bin/pg 1> /dev/null
+  update-alternatives --remove pager /bin/more 1> /dev/null
+  update-alternatives --remove vi /usr/bin/vim.tiny 1> /dev/null
+  update-alternatives --remove view /usr/bin/see 1> /dev/null
+  update-alternatives --remove view /usr/bin/vim.tiny 1> /dev/null
+  update-alternatives --remove vim /usr/bin/vim.basic 1> /dev/null
+  update-alternatives --remove vimdiff /usr/bin/vim.basic 1> /dev/null
+  date +"%F %T" >> /var/log/fg/fgaptitudeclean-alternatives.log
+fi
