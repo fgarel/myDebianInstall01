11d7141c83fee4a7e1a055558ed8bf72a43af890
root <root@debian.mairie.fr>
2012-11-09 10:37:09 +0100

    correction des scripts fgaptitudedo01 et fgaptitudedo02

--- usr/sbin/fgaptitudedo02.sh
+++ usr/sbin/fgaptitudedo02.sh
@@ -3,9 +3,12 @@
 export https_proxy=$http_proxy
 export LANG=fr_FR.UTF-8
 
+mkdir /var/log/fg 2> /dev/null
+
 # chainage avec les scripts de configuration et les mises à jour
 
 fgaptitudeupdate.sh
+fgaptitudeinstallsysframebuffer01.sh
 
 # installation de quelques applications et utilitaires
 # spécifiques à l'administration du système
@@ -18,19 +21,19 @@ fgaptitudeupdate.sh
 # commun
 #fgaptitudeinstallsyscommun02.sh
 # framebuffer
-#fgaptitudeinstallsysframebuffer02.sh
+fgaptitudeinstallsysframebuffer02.sh
 # X
-fgaptitudeinstallsysx02.sh
+#fgaptitudeinstallsysx02.sh
 
 # Mise à jour du système
-fgaptitudeupgrade02.sh
+#fgaptitudeupgrade02.sh
 
 # installation de vim
 #fgaptitudeinstallvim02.sh
 
 # installation de quelques applications et utilitaires
 # spécifiques au domaine du sig
-fgaptitudeinstallgis02.sh
+#fgaptitudeinstallgis02.sh
 
 
 #echo "Récupération des fichiers depuis la machine distante - clefUsb2"
