8f50e604d3d307a942bc6ac4de0b5b74dca2477c
root <root@debian.(none)>
2013-01-09 08:24:03 +0100

    ajout des .vimrc.local

--- usr/sbin/fgaptitudeinstallvim01b.sh
+++ usr/sbin/fgaptitudeinstallvim01b.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
-sh <(curl http://j.mp/spf13-vim3 -L)
-echo colorscheme molokai >> ~/.vimrc.local
-echo \'spf13/vim-colors\' >> ~/.vimrc.bundles.local
+curl http://j.mp/spf13-vim3 -L -o - | sh
+
+# fabrication du fichier ~/.vimrc.local
+#######################################
+echo '" sous-fichier de configuration de l apparence' > ~/.vimrc.local
+echo 'source ~/.vimrc.local.d/theme.vim' >> ~/.vimrc.local
+echo '" sous-fichier de configuration de vos fonctions personnelles par exemple' >> ~/.vimrc.local
+echo 'source ~/.vimrc.local.d/funtions.vim' >> ~/.vimrc.local
+
+# fabrication du sous repertoire ~/.vimrc.local.d
+mkdir ~/.vimrc.local.d
+# theme.vim
+echo '" chargment du theme molokai' > ~/.vimrc.local.d/theme.vim
+echo colorscheme molokai >> ~/.vimrc.local.d/theme.vim
+# functions.vim
+echo '" chargement des fonctions' > ~/.vimrc.local.d/functions.vim
+
+# fabrication du fichier ~/.vimrc.bundles.local
+###############################################
+echo '" Une bonne base d extension quel que soit votre langage de programmation :' > ~/.vimrc.bundles.local
+echo 'let g:spf13_bundle_groups=['general', 'neocomplcache', 'programming', 'python', 'javascript', 'html']' >> ~/.vimrc.bundles.local
+echo 'Budnle 'ZoomWin'' >> ~/.vimrc.bundles.local
+echo 'Bundle 'spf13/vim-colors'' >> ~/.vimrc.bundles.local
