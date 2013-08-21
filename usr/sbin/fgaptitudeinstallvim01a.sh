da7ff85be6c531266bd0f1fa8aaf7906f29fccbd
Frédéric Garel <frederic.garel@gmail.com>
2013-08-21 12:12:01 +0200

    essai vim stable

--- usr/sbin/fgaptitudeinstallvim01a.sh
+++ usr/sbin/fgaptitudeinstallvim01a.sh
@@ -57,9 +57,13 @@ then
   #echo y | aptitude install libperl5.10/stable 1> /dev/null
   #echo y | aptitude install libperl5.10
   #echo y | aptitude install vim-nox 1> /dev/null
-  echo y | aptitude install vim-common/testing 1> /dev/null
-  echo y | aptitude install vim-runtime/testing 1> /dev/null
-  echo y | aptitude install vim-nox/testing 1> /dev/null
+  echo y | aptitude install vim-common 1> /dev/null
+  echo y | aptitude install vim-runtime 1> /dev/null
+  echo y | aptitude install vim-nox 1> /dev/null
+  #echo y | aptitude install vim-common/testing 1> /dev/null
+  #echo y | aptitude install vim-runtime/testing 1> /dev/null
+  #echo y | aptitude install vim-nox/testing 1> /dev/null
+
   echo y | aptitude install exuberant-ctags 1> /dev/null
   #aptitude install vim-scripts
   #echo "y" | aptitude install vim-addon-manager
