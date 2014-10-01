aebc7009375cc3943b98e74608ee5f95e76c2a63
Frédéric Garel <frederic.garel@gmail.com>
2014-10-01 21:06:28 +0200

    bf

--- usr/sbin/bfsys3.sh
+++ usr/sbin/bfsys3.sh
@@ -7,15 +7,15 @@ export LANG=fr_FR.UTF-8
 echo "bfsys3.sh :                    blueflamingo systeme"
 
 # installations pouvant être executées par le debian installer
-aptitude safe-upgrade
+aptitude -y safe-upgrade
 aptitude -y install python3.4 python3.4-venv
 #
 # installation de pyenv (pyenv-installer)
-export PYENV_ROOT="/usr/local/bin/.pyenv" ; curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-export PATH="/usr/local/bin/.pyenv/bin:$PATH"
+export PYENV_ROOT="/opt/.pyenv" ; curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+export PATH="/opt/.pyenv/bin:$PATH"
 
 # installation de pyvenv-virtualenvwrapper
-git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git /usr/local/bin/.pyenv/plugins/pyenv-virtualenvwrapper
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git /opt/.pyenv/plugins/pyenv-virtualenvwrapper
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
